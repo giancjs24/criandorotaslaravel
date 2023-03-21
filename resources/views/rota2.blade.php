659a55a631694d67cffd547ba0590d086fb5e45f
GiancarloSilva <giancjs@gmail.com>
2023-03-21 00:39:06 -0300

    criando rotas em laravel

---
+++ resources/views/rota2.blade.php
@@ -0,0 +1,4 @@
+<h1>Rota 2</h1>
+
+<p><a href="/">Rota 1</a></p>
+<p><a href="/rota3">Rota 3</a></p>
---
+++ resources/views/rota3.blade.php
@@ -0,0 +1,5 @@
+<h1>Rota 3</h1>
+
+<p><a href="/">Rota 1</a></p>
+<p><a href="/rota2">Rota 2</a></p>
+
